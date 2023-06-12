bb00dca923532489bcb25e004e022b3bb55a344d
ColeC <contactcolecarter@gmail.com>
2023-06-11 20:18:13 -0400

    rewrote logic for character generation, the pervious commit did not satisfy project requirements

--- script.js
+++ script.js
@@ -9,13 +9,25 @@ function generatePassword() {
 // Validate the input
 // Generate password
 
-  // const numbers = [0,1,2,3,4,5,6,7,8,9]
-  // const symbols = ["!", "@", "#", "$", "%", "^", "&", "*"]
-  // const characterCodes = Array.from(array(26)).map( (_, i) => i+97);
-  // console.log(characterCodes);
+  const numbers = [0,1,2,3,4,5,6,7,8,9]
+  const symbols = ["!", "@", "#", "$", "%", "^", "&", "*"]
+  const characterCodes = Array.from(Array(26)).map( (_, i) => i+97);
+  const lowercaseLetters = characterCodes.map(code => String.fromCharCode(code));
+  const uppercaseLetters = lowercaseLetters.map(letter => letter.toUpperCase())
+
+  const generatePassword = (length, hasNumbers, hasSymbols, hasLowercase, hasUppercase) => {
+    const availableCharacters = [
+      ...(hasNumbers ? numbers : []),
+      ...(hasSymbols ? symbols : []),
+      ...(hasLowercase ? lowercaseLetters : []),
+      ...(hasUppercase ? uppercaseLetters : []),
+    ]
+  }
+  console.log(availableCharacters);
 
     return "generated password will go here"
 }
+generatePassword(12, true, true, true, true);
 
 // Write password to the #password input
 function writePassword() {
